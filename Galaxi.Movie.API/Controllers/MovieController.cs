e80e75d2c837ef0b98cd246427ffd1dd024fe599
JDVega6 <juanguativa07@gmail.com>
2024-01-07 04:19:14 -0500

    Build Structure

---
+++ Galaxi.Movie.API/Controllers/MovieController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Galaxi.Movie.API.Controllers
+{
+    public class MovieController : ControllerBase
+    {
+        public MovieController() { }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+           return NoContent();
+        }
+    }
+}
