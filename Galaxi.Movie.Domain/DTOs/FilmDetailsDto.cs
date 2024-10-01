ac8b3d92de2ebd19f6e745bdd343943b959eb6b5
JDVega6 <juanguativa07@gmail.com>
2024-10-01 11:14:04 -0500

    Add: Trailer attribute to the Film model

--- Galaxi.Movie.Domain/DTOs/FilmDetailsDto.cs
+++ Galaxi.Movie.Domain/DTOs/FilmDetailsDto.cs
@@ -5,6 +5,7 @@
         public Guid FilmId { get; set; }
         public string Title { get; set; }
         public string? PosterImage { get; set; }
+        public string? Trailer { get; set; }
         public string? Description { get; set; }
         public string? Director { get; set; }
         public string? Genre { get; set; }
