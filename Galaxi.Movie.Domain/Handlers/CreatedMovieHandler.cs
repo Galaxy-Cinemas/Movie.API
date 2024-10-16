931ed16b1dd0769ee2fb7e984768d93d8deab26d
JDVega6 <juanguativa07@gmail.com>
2024-10-16 18:19:15 -0500

    Add CRUD

--- Galaxi.Movie.Domain/Handlers/CreatedMovieHandler.cs
+++ Galaxi.Movie.Domain/Handlers/CreatedMovieHandler.cs
@@ -1,8 +1,10 @@
 ﻿using AutoMapper;
+using Galaxi.Bus.Message;
 using Galaxi.Movie.Data.Models;
 using Galaxi.Movie.Domain.DTOs;
 using Galaxi.Movie.Domain.Infrastructure.Commands;
 using Galaxi.Movie.Persistence.Repositorys;
+using MassTransit;
 using MediatR;
 
 namespace Galaxi.Movie.Domain.Handlers
@@ -13,11 +15,13 @@ namespace Galaxi.Movie.Domain.Handlers
     {
         private readonly IMovieRepository _repo;
         private readonly IMapper _mapper;
+        private readonly IBus _bus;
 
-        public CreatedMovieHandler(IMovieRepository repo, IMapper mapper)
+        public CreatedMovieHandler(IMovieRepository repo, IMapper mapper, IBus bus)
         {
             _repo = repo;
             _mapper = mapper;
+            _bus = bus;
         }
 
         public async Task<CreatedFilmReponseDTO> Handle(CreatedMovieCommand request, CancellationToken cancellationToken)
@@ -27,6 +31,8 @@ namespace Galaxi.Movie.Domain.Handlers
             
             var sucess = await _repo.SaveAll();
             var filmResponse = _mapper.Map<CreatedFilmReponseDTO>(createdMovie);
+
+            await _bus.Publish(new AddMovie { film = createdMovie });
 
             if (!sucess)
             {
