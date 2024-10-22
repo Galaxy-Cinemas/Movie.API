bb75577616cf8047a39e49e57c90b270893a339f
JDVega6 <juanguativa07@gmail.com>
2024-10-22 10:03:47 -0500

    Remove: Unused GET endpoints removed

--- Galaxi.Movie.API/Controllers/MovieController.cs
+++ Galaxi.Movie.API/Controllers/MovieController.cs
@@ -46,58 +46,6 @@ namespace Galaxi.Movie.API.Controllers
             return StatusCode(successResponse.StatusCode.Value, successResponse);
         }
 
-        [HttpGet]
-        [AllowAnonymous]
-        public async Task<IActionResult> GetAllMovies()
-        {
-            try
-            {
-                _log.LogDebug("RequestStart - [GET] /movies");
-                var movies = await _mediator.Send(new GetAllMoviesQuery());
-                var successResponse = ResponseHandler<IEnumerable<FilmSummaryDTO>>.SuccessResponse("Movie retrieved successfully", movies);
-                _log.LogInformation("Movie retrieved successfully Info");
-                return StatusCode(successResponse.StatusCode.Value, successResponse);
-            }
-            catch (KeyNotFoundException ex)
-            {
-                _log.LogWarning(ex.Message);
-                var response = ResponseHandler<string>.NotFoundResponse("Movies not found.", ex.Message);
-                return StatusCode(response.StatusCode.Value, response);
-            }
-            catch (Exception ex)
-            {
-                _log.LogError(ex,ex.Message);
-                var errorResponse = ResponseHandler<string>.ErrorResponse("An internal server error occurred", ex);
-                return StatusCode(errorResponse.StatusCode.Value, errorResponse);
-            }
-        }
-
-        [HttpGet("{filmId}")]
-        [AllowAnonymous]
-        public async Task<IActionResult> GetByMovieId(Guid filmId)
-        {
-            try
-            {
-                _log.LogDebug("Processing movie with Id: {filmId}", filmId);
-                var movie = await _mediator.Send(new GetMovieByIdQuery(filmId));
-                var successResponse = ResponseHandler<FilmDetailsDTO>.SuccessResponse("Movie created successfully", movie);
-                _log.LogInformation($"Successfully processed MovieCreated event for MovieId: {movie.FilmId}");
-                return StatusCode(successResponse.StatusCode.Value, successResponse);
-            }
-            catch (InvalidOperationException ex)
-            {
-                _log.LogWarning(ex.Message);
-                var errorResponse = ResponseHandler<string>.ErrorResponse("Failed to save changes to the database.", ex);
-                return StatusCode(errorResponse.StatusCode.Value, errorResponse);
-            }
-            catch (Exception ex)
-            {
-                _log.LogError(ex.Message);
-                var errorResponse = ResponseHandler<string>.ErrorResponse("An internal server error occurred", ex);
-                return StatusCode(errorResponse.StatusCode.Value, errorResponse);
-            }
-        }
-
         [HttpPost]
         public async Task<IActionResult> CreateMovie([FromBody] CreatedMovieCommand movieToCreate)
         {
